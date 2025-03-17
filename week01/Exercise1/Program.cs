6415649ac26c20cff743aa208283bccd1ada11e6
Olaa025 <odukoyaolaleye2311@gmail.com>
2025-03-17 18:54:11 +0100

    week one assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your fristname.");
+        string fristname = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine($"My name is: {lastname}, {fristname} {lastname}");
+        
     }
 }
